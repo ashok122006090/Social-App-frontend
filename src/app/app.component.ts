176bf7e31c67c201f32494efeaf70422d28a98c7
ashok122006090 <yellaashok8@gmail.com>
2025-04-03 09:02:49 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Message {
+  name: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'social';
+export class AppComponent implements OnInit {
+  messages: Message[] = [];
+  newMessage: Message = { name: '', text: '' };
+  apiUrl = 'http://localhost:8080/messages';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchMessages();
+  }
+
+  fetchMessages() {
+    this.http.get<Message[]>(this.apiUrl).subscribe(data => {
+      this.messages = data;
+    });
+  }
+
+  sendMessage() {
+    if (this.newMessage.name && this.newMessage.text) {
+      this.http.post<Message>(this.apiUrl, this.newMessage).subscribe(() => {
+        this.fetchMessages();
+        this.newMessage = { name: '', text: '' }; // Clear input fields
+      });
+    }
+  }
 }
